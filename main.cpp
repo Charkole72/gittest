c865dd900915f3af26d17bdca5a19c827a1f0de4
Charkole <yumyintmyataung.ec@gmail.com>
2024-01-23 14:36:03 +0630

    Version 1.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main()
 {
-	std::cout<<"Hello"<<endl;
+	std::cout<<"Hello"<<std::endl;
 }
---
+++ main1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std:: cout<<"Nice to meet you!"<<std::endl;
+    return 0;
+}
